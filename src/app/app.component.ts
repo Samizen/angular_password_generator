b8aa1edf7c9dfc706d1917c254d4a567623f3634
Samit Paudel <samitpdl@gmail.com>
2022-02-22 15:57:57 +0545

    Add whole application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  // Property:
+  length: number = 0;
+  password: string = '';
+  hasLetters: boolean =  false;
+  hasNumbers: boolean = false;
+  hasSpecialCharacters: boolean = false;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.hasLetters = !this.hasLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.hasNumbers = !this.hasNumbers;
+  }
+
+  onChangeUseSpecialCharacters() {
+    this.hasSpecialCharacters = !this.hasSpecialCharacters;
+  }
+
+  // Function to run anytime a user clicks the button
+  onButtonClick() {
+    const numbers = '0123456789'
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const specialCharacters = '!@#$%^&*()_-+:'
+
+    let validChars = '';
+
+    if(this.hasLetters){
+      validChars += letters
+    }
+    if(this.hasNumbers){
+      validChars += numbers
+    }
+    if(this.hasSpecialCharacters){
+      validChars += specialCharacters 
+    }
+
+    let generatedPassword = ''
+    for(let i=0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length); 
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+    
+  }
 }
